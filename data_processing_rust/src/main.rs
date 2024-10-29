465393014f76437495acc8d99cfae1d0ab8b1874
aoaow <76165158+aoaow@users.noreply.github.com>
2024-10-29 00:50:05 +0000

    1

--- data_processing_rust/src/main.rs
+++ data_processing_rust/src/main.rs
@@ -1,9 +1,22 @@
 // src/main.rs
 use std::error::Error;
 use data_processing_rust::process_data;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    process_data("./python_script_with_rust/data/input.csv", 
-    "./python_script_with_rust/output.csv", "Fare")?;
+
+    let mut input_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    input_path.pop()
+    input_path.push("data/input.csv")
+
+    let mut output_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    output_path.pop(); // Move up one directory level
+    output_path.push("output.csv");
+
+    process_data(
+        input_path.to_str().unwrap(),
+        output_path.to_str().unwrap(),
+        "Fare",
+    )?;
     Ok(())
 }
