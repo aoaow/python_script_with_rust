42b8e829c67ea21a3ff8e4f76af5cd479f86ca9e
aoaow <76165158+aoaow@users.noreply.github.com>
2024-10-29 00:34:55 +0000

    1

--- data_processing_rust/src/main.rs
+++ data_processing_rust/src/main.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use data_processing_rust::process_data;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    process_data("python_script_with_rust/data/input.csv", 
-    "python_script_with_rust/output.csv", "Fare")?;
+    process_data("./python_script_with_rust/data/input.csv", 
+    "./python_script_with_rust/output.csv", "Fare")?;
     Ok(())
 }
